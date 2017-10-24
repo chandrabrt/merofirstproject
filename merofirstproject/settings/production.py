2534305dfd265569231adc1dbd3830ca81d99fba
chandrabrt <chandra2khadka4@gmail.com>
2017-10-24 20:35:11 +0545

    first commitss

--- merofirstproject/settings/production.py
+++ merofirstproject/settings/production.py
@@ -114,20 +114,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_in_pro"),
-    # os.path.join(BASE_DIR, "static_in_env"),
-    # '/var/www/static/',
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
-# Crispy form tags Settings
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # LOGOUT_URL = 'accounts:logout'
@@ -190,7 +189,7 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_SECONDS = 1000000
 SECURE_FRAME_DENY = True
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
